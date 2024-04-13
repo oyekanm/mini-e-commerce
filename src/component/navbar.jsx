7e2f417a1cedd870745f4428247bcb45ce9a8b7c
Oyekanmi <64901642+oyekanm@users.noreply.github.com>
2024-04-13 16:09:34 +0100

    Update navbar.jsx

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -120,7 +120,7 @@ export default function Navbar() {
                     pathname === item.href
                       ? "bg-gray-900 text-white"
                       : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                    "block rounded-md px-3 py-2 text-base font-medium"
+                    "block rounded-md px-3 py-2 text-[2rem] font-medium"
                   )}
                   aria-current={pathname === item.href ? "page" : undefined}
                 >
